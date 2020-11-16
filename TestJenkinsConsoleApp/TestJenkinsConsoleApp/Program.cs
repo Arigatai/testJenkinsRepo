bb9707e2224cd25390733523f309c2ddc1393c89
Arigatai <arigatai.144@gmail.com>
2020-11-16 15:41:26 +0300

    Создал тестовое приложение

---
+++ TestJenkinsConsoleApp/TestJenkinsConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TestJenkinsConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is test app\n");
+
+            Console.Write("Enter first number: ");
+            var n1 = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Enter secondNumbe: ");
+            var n2 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine($"Result = {n1 + n2}");
+            Console.ReadKey();
+        }
+    }
+}
